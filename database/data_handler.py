2bfbc996cf82e6ac7896b2a9020d194be1dc6f08
NickolayS1 <nik.electroboy@gmail.com>
2024-12-03 18:12:15 +0300

    data_handler functions description added

--- database/data_handler.py
+++ database/data_handler.py
@@ -8,28 +8,55 @@ class DataBase:
         pass
 
     def get_type_of_group(self, group_id: int) -> DataBaseResponses:
+        """
+        Gets the type of the group via group_id
+        """
         pass
 
     def set_type_of_group(self, group_id: int, group_type: GroupTypes) -> DataBaseResponses:
+        """
+        Sets new type of the group via group_id
+        """
         pass
 
     def get_user_type(self, user_id: int, group_id: int) -> UserTypes:
+        """
+        Gets user type in a specified group via user_id and group_id
+        """
         pass
 
     def set_user_type(self, user_id: int, group_id: int, new_type: UserTypes) -> DataBaseResponses:
+        """
+        Sets new user type in a specified group via user_id and group_id
+        """
         pass
 
     def get_user_id_list(self, group_id: int) -> list[int]:
+        """
+        Gets user_id list from the specified group via group_id
+        """
         pass
     
     def get_amount_of_maximum_warns(self, group_id: int) -> int:
+        """
+        Gets the maximum number of warns before users get banned
+        """
         pass
 
     def set_amount_of_maximum_warns(self, group_id: int) -> int:
+        """
+        Sets the maximum number of warns before users get banned
+        """
         pass
 
     def get_amounts_of_warns(self, user_id: int, group_id: int) -> int:
+        """
+        Gets the number of warns of the specified user via user_id and group_id
+        """
         pass
 
     def get_groups_list(self, user_id: int) -> list[int]:
+        """
+        Gets the list of [group_id] for each group that uses this Telegram bot
+        """
         pass
